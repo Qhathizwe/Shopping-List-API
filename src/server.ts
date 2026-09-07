5a023707b22fe2ce5e61ebf314111c924516fe3e
qhathizwe <sphoswa2@gmail.com>
2026-09-07 13:27:13 +0200

    server alterations

--- src/server.ts
+++ src/server.ts
@@ -6,15 +6,6 @@ const Port = 4000;
 const requestListener = (req: IncomingMessage, res: ServerResponse) => {
     console.log(req.url, 'url');
 
-    
-    // if (req.url?.startsWith('/items')) {
-    //     ItemsRoute(req, res);
-    //     return;
-    // }else{
-    //     res.writeHead(200, {"Content-Type": "application/json"});
-    //     res.end(JSON.stringify({ message: "Hello, World!" }));
-    // }
-    // }
 
 
 const server = http.createServer(requestListener);
