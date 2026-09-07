126f7090ebb6b4adbbd3e68d25cfec9f2e73ff96
qhathizwe <sphoswa2@gmail.com>
2026-09-07 14:14:49 +0200

    setting isPuchased current state to false unil you have to update it when you have purchased it

--- src/controllers/list.ts
+++ src/controllers/list.ts
@@ -14,7 +14,7 @@ export const getItemById = (id: number): listItem | undefined => {
 }
 
 export const addItem = (name: string, quantity: number, category: string, notes: string, isPurchased: boolean): listItem => {
-    const newItem: listItem = { id: currentId++, name, quantity, category, notes, isPurchased };
+    const newItem: listItem = { id: currentId++, name, quantity, category, notes, isPurchased: false };
     List.push(newItem);
     return newItem;
 }
