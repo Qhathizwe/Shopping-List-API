7898a4be1c5aacbd0abd72f47bdfab4e658a82e1
qhathizwe <sphoswa2@gmail.com>
2026-09-07 13:29:50 +0200

    importing itemsRouter to the server and intergrating it

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
 import http, { IncomingMessage, ServerResponse } from "http";
-// import { ItemsRoute } from "./routes/List.js";
+import { ItemsRoute } from "./routes/List.js";
 
 const Port = 4000;
 
 const requestListener = (req: IncomingMessage, res: ServerResponse) => {
     console.log(req.url, 'url');
 
+    
+    if (req.url?.startsWith('/items')) {
+        ItemsRoute(req, res);
+        return;
+    }else{
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({ message: "Hello, World!" }));
+    }
+    }
 
 
 const server = http.createServer(requestListener);
